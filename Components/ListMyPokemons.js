622fbb291f0005e2d486afc7fbbc4d5ab72be1ee
Leon Cruz <leon.cruz.teixeira@gmail.com>
2018-07-07 19:38:43 -0300

    Criação do arquivo que ira gerenciar a lista de pokemons favoritos

---
+++ Components/ListMyPokemons.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+
+export default class ListMyPokemons extends Component<Props> {
+    render() {
+        return(
+            <View>
+                
+            </View>
+        );
+    }
+}
