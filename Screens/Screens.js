1227580480ee4012970b6f428f072bdc4bda6f67
Leon Cruz <leon.cruz.teixeira@gmail.com>
2018-07-07 00:44:54 -0300

    Simplificação na criação do gerenciador das telas

--- Screens/Screens.js
+++ Screens/Screens.js
@@ -2,8 +2,10 @@ import { createStackNavigator } from 'react-navigation';
 import HomeScreen from './HomeScreen.js';
 
 
-export default createStackNavigator({
-    Home: {
-        screen: HomeScreen
+const Screens = createStackNavigator(
+    {
+        Home: HomeScreen,
     },
-});
+);
+
+export default Screens;
