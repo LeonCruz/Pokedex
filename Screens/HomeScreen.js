5e6236364bf09ddbed1e216f5d4e7d1398fbf0e6
Leon Cruz <leon.cruz.teixeira@gmail.com>
2018-07-07 19:53:58 -0300

    Troca no nome da variável

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -12,27 +12,27 @@ export default class HomeScreen extends Component<Props> {
         my_pokemons : [
             {
                 id: 1,
-                nome: 'Pokémon 1',
+                name: 'Pokémon 1',
                 sprite: 'https://raw.githubuserco…er/sprites/pokemon/1.png',
             },
             {
                 id: 2,
-                nome: 'Pokémon 2',
+                name: 'Pokémon 2',
                 sprite: 'https://raw.githubuserco…er/sprites/pokemon/2.png',
             },
             {
                 id: 3,
-                nome: 'Pokémon 3',
+                name: 'Pokémon 3',
                 sprite: 'https://raw.githubuserco…er/sprites/pokemon/3.png',
             },
             {
                 id: 4,
-                nome: 'Pokémon 4',
+                name: 'Pokémon 4',
                 sprite: 'https://raw.githubuserco…er/sprites/pokemon/4.png',
             },
             {
                 id: 5,
-                nome: 'Pokémon 5',
+                name: 'Pokémon 5',
                 sprite: 'https://raw.githubuserco…er/sprites/pokemon/5.png',
             },
         ],
@@ -41,7 +41,7 @@ export default class HomeScreen extends Component<Props> {
     render() {
         return (
             <View style={styles.container}>
-                <Text>Olá</Text>
+                <ListMyPokemons my_pokemons={this.state.my_pokemons}/>
             </View>
         );
     }
