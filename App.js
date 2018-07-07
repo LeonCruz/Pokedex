5605807214ec92b53d53585389be9d2138082808
Leon Cruz <leon.cruz.teixeira@gmail.com>
2018-07-07 00:37:32 -0300

    Retirada do compomente 'View'

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import  Screens from './Screens/Screens.js';
+import Screens from './Screens/Screens.js';
 
 
 export default class App extends Component<Props> {
     render() {
         return (
-            <View>
-                <Screens />                
-            </View>
+            <Screens />
         );
     }
 }
