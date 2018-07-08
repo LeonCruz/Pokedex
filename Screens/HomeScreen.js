8205a16571de7eb8d0403af3cfe02f7b2837ea71
Leon Cruz <leon.cruz.teixeira@gmail.com>
2018-07-08 10:30:03 -0300

    Alteração no link dos sprites

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -13,27 +13,27 @@ export default class HomeScreen extends Component<Props> {
             {
                 id: 1,
                 name: 'Pokémon 1',
-                sprite: 'https://raw.githubuserco…er/sprites/pokemon/1.png',
+                sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
             },
             {
                 id: 2,
                 name: 'Pokémon 2',
-                sprite: 'https://raw.githubuserco…er/sprites/pokemon/2.png',
+                sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png',
             },
             {
                 id: 3,
                 name: 'Pokémon 3',
-                sprite: 'https://raw.githubuserco…er/sprites/pokemon/3.png',
+                sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png',
             },
             {
                 id: 4,
                 name: 'Pokémon 4',
-                sprite: 'https://raw.githubuserco…er/sprites/pokemon/4.png',
+                sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
             },
             {
                 id: 5,
                 name: 'Pokémon 5',
-                sprite: 'https://raw.githubuserco…er/sprites/pokemon/5.png',
+                sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png',
             },
         ],
     }
@@ -41,6 +41,7 @@ export default class HomeScreen extends Component<Props> {
     render() {
         return (
             <View style={styles.container}>
+                <Text>Meus Pokémons</Text>
                 <ListMyPokemons my_pokemons={this.state.my_pokemons}/>
             </View>
         );
