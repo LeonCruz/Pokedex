ed704317b46ee8e8e6305666fa37a58d113d334c
Leon Cruz <leon.cruz.teixeira@gmail.com>
2018-07-08 16:08:01 -0300

    Recebimento do pokémon selecionado como parâmentro

--- Screens/MyPokemonScreen.js
+++ Screens/MyPokemonScreen.js
@@ -1,11 +1,19 @@
-import Reac, { Component } from 'react';
+import React, { Component } from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 
 export default class MyPokemonScreen extends Component {
+    static navigationOptions = {
+        title: 'Pokémon Profile',
+    }
+
     render() {
+        const { navigation } = this.props;
+        const pokemon = navigation.getParam('pokemon', 'Sem Nome');
+
         return(
             <View>
+                <Text>{pokemon.name}</Text>
                 <Text>Olá</Text>
             </View>
         );
