791ec36726084fc8946e1cc25bbdd9048599d760
Leon Cruz <leon.cruz.teixeira@gmail.com>
2018-07-08 14:51:00 -0300

    Criação da tela do perfil dos pokémons favoritos

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+
 import Screens from './Screens/Screens';
+import NavigationService from './Components/NavigationService';
 
 
 export default class App extends Component<Props> {
     render() {
         return (
-            <Screens />
+            <Screens
+                ref={navigator_ref => {
+                    NavigationService.setTopLevelNavigator(navigator_ref)
+                }}/>
         );
     }
 }
---
+++ Screens/MyPokemonProfile.js
@@ -0,0 +1,13 @@
+import Reac, { Component } from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+
+export default class MyPokemonProfile extends Component {
+    render() {
+        return(
+            <View>
+                <Text>Olá</Text>
+            </View>
+        );
+    }
+}
