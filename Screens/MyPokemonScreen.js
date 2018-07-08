19acfa861b0cae9590956381b1982517d10f8290
Leon Cruz <leon.cruz.teixeira@gmail.com>
2018-07-08 14:52:48 -0300

    Troca no nome do arquivo

--- Screens/MyPokemonScreen.js
+++ Screens/MyPokemonScreen.js
@@ -2,7 +2,7 @@ import Reac, { Component } from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 
-export default class MyPokemonProfile extends Component {
+export default class MyPokemonScreen extends Component {
     render() {
         return(
             <View>
