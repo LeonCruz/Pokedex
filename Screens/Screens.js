095b65a2ab3070fb4f1ba3c05954f5ae8ea366ed
Leon Cruz <leon.cruz.teixeira@gmail.com>
2018-07-08 14:53:03 -0300

    Adição da 'MyPokemonScreen' a lista de telas.

--- Screens/Screens.js
+++ Screens/Screens.js
@@ -1,11 +1,16 @@
 import { createStackNavigator } from 'react-navigation';
 import HomeScreen from './HomeScreen';
+import MyPokemonScreen from './MyPokemonScreen';
 
 
 const Screens = createStackNavigator(
     {
         Home: HomeScreen,
+        MyPokemon: MyPokemonScreen,
     },
+    {
+        initialRoute: Home,
+    }
 );
 
 export default Screens;
