97baf6beea352d2fe5b877fcc271be09cf4d62df
Leon Cruz <leon.cruz.teixeira@gmail.com>
2018-07-18 15:20:28 -0300

    Adição do campo de texto para informar o nome do pokémon.

--- Screens/SearchPokemonScreen.js
+++ Screens/SearchPokemonScreen.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, TextInput} from 'react-native';
 
 
 export default class SearchPokemonScreen extends Component {
+    static navigationOptions = {
+        headerTitle: <TextInput
+                        placeholder="Nome do Pokémon"
+                        autoFocus={true}
+                        style={{flex: 1, fontSize: 18}}/>
+
+    };
+
     render() {
         return(
             <View>
