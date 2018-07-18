21f67c6227b2a17df2c705ea0b65d4bcf7241272
Leon Cruz <leon.cruz.teixeira@gmail.com>
2018-07-18 14:39:52 -0300

    Adição da nova tela na rota

--- Screens/Screens.js
+++ Screens/Screens.js
@@ -1,12 +1,15 @@
 import { createStackNavigator } from 'react-navigation';
+
 import HomeScreen from './HomeScreen';
 import MyPokemonScreen from './MyPokemonScreen';
+import SearchPokemonScreen from './SearchPokemonScreen';
 
 
 const Screens = createStackNavigator(
     {
         Home: HomeScreen,
         MyPokemon: MyPokemonScreen,
+        SearchPokemon: SearchPokemonScreen,
     },
     {
         initialRoute: 'Home',
