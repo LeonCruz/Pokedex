5e5f5c707398bb08eb225bfb82ebbb4acaa13e1a
Leon Cruz <leon.cruz.teixeira@gmail.com>
2018-07-18 15:06:41 -0300

    Adição da função para chamar a página de busca

--- Components/SearchIcon.js
+++ Components/SearchIcon.js
@@ -2,12 +2,15 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import NavigationService from '../Components/NavigationService';
 
 
 export default class SearchIcon extends Component {
     render() {
         return(
-            <TouchableOpacity style={{marginRight: 10}}>
+            <TouchableOpacity
+                style={{marginRight: 10}}
+                onPress={() => {NavigationService.navigate('SearchPokemon', {})} }>
                 <Icon name="search" size={30}
                     color="#000" backgroundColor="#fff"/>
             </TouchableOpacity>
