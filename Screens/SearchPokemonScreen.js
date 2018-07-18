7989f4e7b80872a9d1af4d5740727037a23444c1
Leon Cruz <leon.cruz.teixeira@gmail.com>
2018-07-18 14:39:04 -0300

    Correção no nome da classe.

--- Screens/SearchPokemonScreen.js
+++ Screens/SearchPokemonScreen.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 
-export default class SearchPokemon extends Component {
+export default class SearchPokemonScreen extends Component {
     render() {
         return(
             <View>
-                
+
             </View>
         );
     }
