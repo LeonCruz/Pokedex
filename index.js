8e63aa8cd96f24a458aef64305f3a8ea4507ed30
Leon Cruz <leon.cruz.teixeira@gmail.com>
2018-08-15 01:02:28 -0300

    Ignorar os avisos

--- index.js
+++ index.js
@@ -3,5 +3,11 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {YellowBox} from 'react-native';
+
+YellowBox.ignoreWarnings([
+    'source.uri should not be an empty string',
+    'Setting a timer for a long period of time'
+]);
 
 AppRegistry.registerComponent(appName, () => App);
