db45998717e436b3856739ffe7e454f0e3d729ff
Leon Cruz <leon.cruz.teixeira@gmail.com>
2018-08-15 14:25:45 -0300

    Remoção da lista estática de pokémons

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -17,53 +17,7 @@ export default class HomeScreen extends Component<Props> {
     };
 
     state = {
-        my_pokemons : [
-            {
-                id: 1,
-                name: 'Pokémon 1',
-                sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
-            },
-            {
-                id: 2,
-                name: 'Pokémon 2',
-                sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png',
-            },
-            {
-                id: 3,
-                name: 'Pokémon 3',
-                sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png',
-            },
-            {
-                id: 4,
-                name: 'Pokémon 4',
-                sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
-            },
-            {
-                id: 5,
-                name: 'Pokémon 5',
-                sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png',
-            },
-            {
-                id: 6,
-                name: 'Pokémon 6',
-                sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
-            },
-            {
-                id: 7,
-                name: 'Pokémon 7',
-                sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png',
-            },
-            {
-                id: 8,
-                name: 'Pokémon 8',
-                sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png',
-            },
-            {
-                id: 9,
-                name: 'Pokémon 9',
-                sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png',
-            },
-        ],
+        my_pokemons : [],
     }
 
     render() {
